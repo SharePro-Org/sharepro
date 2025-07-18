ea1768fe6ec00b779fd6a1afe71dbbe3e00e8a9d
iamalaboexcel <iamalaboexcel@gmail.com>
2025-07-18 17:31:23 +0100

    deployment

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -16,7 +16,7 @@ export const setToken = (token: string): void => {
   localStorage.setItem('accessToken', token);
 };
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/graphql/";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.mysharepro.com/graphql/";
 if (!API_BASE_URL) {
   throw new Error(
     'NEXT_PUBLIC_API_URL is not set in environment variables.'
