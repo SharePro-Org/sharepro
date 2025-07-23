594f81a6af064487d34d5b2c9f30fdc4fa03a740
iamalaboexcel <iamalaboexcel@gmail.com>
2025-07-23 12:51:05 +0100

    feat: implement rewards page with navigation button

--- src/app/business/campaigns/create/rewards/page.tsx
+++ src/app/business/campaigns/create/rewards/page.tsx
@@ -1,10 +1,24 @@
+"use client"
+
 import DashboardLayout from '@/components/dashboard/DashboardLayout';
 import React from 'react';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const rewards = () => {
+  const router = useRouter();
+
   return (
     <DashboardLayout>
-      <></>
+      <section className='bg-white rounded-md md:p-6 p-3'>
+        <button
+          className='text-black cursor-pointer flex items-center'
+          onClick={() => router.back()}
+        >
+          <ArrowLeft className="mr-3" />
+          <span className='text-lg font-semibold capitalize'>Define Campaign Rewards</span>
+        </button>
+      </section>
     </DashboardLayout>
   );
 };
